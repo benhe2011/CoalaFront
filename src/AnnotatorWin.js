09f6c6be1bab75e3232c342fe1c4661a8645489b
Benjamin Chen He <benhe@vpn-172-27-2-75.host.ucla.edu>
2020-10-26 10:07:19 -0700

    First commit

---
+++ src/AnnotatorWin.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactImageAnnotate from "react-image-annotate";
+
+const AnnotatorWin = (props) => {
+  if(props.imgSrc){
+    return (
+    <ReactImageAnnotate
+      labelImages
+      regionClsList={["Alpha", "Beta", "Charlie", "Delta"]}
+      regionTagList={["tag1", "tag2", "tag3"]}
+      images={[
+        {
+          src: props.imgSrc,
+          name: props.imgName,
+          regions: []
+        }
+      ]}
+      enabledTools={["create-point", "select"]}
+    />
+    );
+  }
+  else {
+    return (
+    <ReactImageAnnotate
+      labelImages
+      regionClsList={["Alpha", "Beta", "Charlie", "Delta"]}
+      regionTagList={["tag1", "tag2", "tag3"]}
+      images={[
+        {
+          src: "./public/tempimg1.png",
+          name: "Image 1",
+          regions: []
+        }
+      ]}
+    />
+    );
+  }
+}
+
+export default AnnotatorWin
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactImageAnnotate from "react-image-annotate";
+import ReactDOM from "react-dom";
+import AnnotatorWin from "./AnnotatorWin"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    //this.state = {imgSrc: null, imgName: null};
+    this.state = {imgSrc: null}
+  }
+  
+  
+  fileChangedHandler = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      var imageSrc = null;
+      const reader = new FileReader();
+      reader.onload = (e) => {
+          this.setState({imgSrc: e.target.result})
+          //imageSrc = e.target.result;
+      }
+      reader.readAsDataURL(file);
+      //this.setState({imgSrc: imageSrc, imgName: file.name})
+    }
+  }
+  
+  render(){
+    if(this.state.imgSrc){
+      return (
+        <div className="App">
+          {/* <input type="file" accept="image/*" onChange={this.fileChangedHandler} /> */}
+          <AnnotatorWin imgSrc={this.state.imgSrc} />;
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <input type="file" accept="image/*" onChange={this.fileChangedHandler} />
+      </div>
+    );
+  }
+  // return <AnnotatorWin />;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'))
